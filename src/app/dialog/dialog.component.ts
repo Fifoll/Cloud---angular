03933a970bd5eefbf5f2b98bd663b2b531a408b7
Fifoll <kakolfilip@wp.pl>
2024-01-14 16:21:14 +0100

    add body as html template

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { DialogData } from '../dialog-data';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dialog',
@@ -11,6 +12,7 @@ export class DialogComponent {
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private sanitizer: DomSanitizer
   ) {}
 
   close(status?: boolean): void {
@@ -20,5 +22,9 @@ export class DialogComponent {
     else {
       this.dialogRef.close();
     }
+  }
+
+  getHTML(body: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(body);
   }
 }
