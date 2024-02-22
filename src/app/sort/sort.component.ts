0c4d3995d5ad977393fe1d623bb62d4402753060
Fifoll <kakolfilip@wp.pl>
2024-02-22 18:42:38 +0100

    emit sort object

--- src/app/sort/sort.component.ts
+++ src/app/sort/sort.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatSelectChange } from '@angular/material/select';
 
@@ -8,18 +8,31 @@ import { MatSelectChange } from '@angular/material/select';
   styleUrls: ['./sort.component.css']
 })
 export class SortComponent {
-  selectedValue: boolean = false;
+  selectedValue: string = '';
   order: boolean = true;
+
+  @Output() sortFilesEvent = new EventEmitter<object | null>();
 
   sortForm = new FormGroup({
     sort: new FormControl()
   });
 
   sortFiles(event: MatSelectChange) {
-    this.selectedValue = true;
-    console.log("Wybrane wartość:", event.value);
+    this.selectedValue = event.value;
+    this.emitEvent();
   }
+
   changeOrder() {
     this.order = !this.order;
+    this.emitEvent();
+  }
+
+  emitEvent() {
+    const options = {
+      field: this.selectedValue,
+      order: this.order
+    };
+    this.sortFilesEvent.emit(options);
+    console.log(options);
   }
 }
